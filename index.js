fbcb85de312f4e9ada5e1f9d08c277c78fa8c4c0
Manikrishnaa <manikrishna.9970@gmail.com>
2024-04-05 11:36:36 +0530

    updated

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
 const userdatamodel = require("./models/userdataschema");
 
 const app = express();
 app.set('view engine','ejs');
 app.use(bodyparser.urlencoded({extended : true}));
 
+mongoose.connect('mongodb://localhost:27017/grocery')
+    .then(()=>{
+        console.log("mongodb connected");
+    })
+    .catch((error) => {
+        console.error("MongoDB connection error:", error);
+    });
+
 app.get('/',(req,res)=>{
     
 });
 
 app.get('/signup',(req,res)=>{
-    res.render('signup');
+    res.render('signup',{emailexists : false,mobileexists : false});
 });
 
 app.get('/login',(req,res)=>{
     res.render('login');
 });
 
-app.post('/signup',(req,res)=>{
-
+app.post('/signup',async(req,res)=>{
+    const {name,email,mobile,address,password} = req.body;
+    let checkemail = await userdatamodel.findOne({email : email});
+    if(checkemail){
+        res.render("signup",{emailexists : true,mobileexists : false});
+    }
+    if(checkmobile){
+        res.render("signup",{emailexists : false,mobileexists : true});
+    }
+    const newuser = new userdatamodel({name : name,email : email,mobile : mobile,address : address,password : password});
+    await newuser.save();
 });
 
 app.post('/login',(req,res)=>{
--- models/userdataschema.js
+++ models/userdataschema.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/grocery')
-
 const userdataschema = new mongoose.Schema({
     name : {type : String,required : true},
     email : {type : String,required : true,unique : true},
