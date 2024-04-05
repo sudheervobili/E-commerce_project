493b9d9afb006ed35f38b0cd7e78abbf76a536c5
sudheervobili <sudheervobillisetty2003@gmail.com>
2024-04-05 12:10:33 +0530

    new changes made in index.js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require('express');
-const bodyparser = require('body-parser');
+const express = require("express");
+const bodyparser = require("body-parser");
+const path = require("path");
 
 const app = express();
-app.set('view engine','ejs');
-app.use(bodyparser.urlencoded({extended : true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyparser.urlencoded({ extended: true }));
 
-app.post('/signup',(req,res)=>{
-
+app.get("/", (req, res) => {
+	res.send("Hello world");
 });
 
-app.post('/login',(req,res)=>{
-    
+app.get("/signup", (req, res) => {
+	res.render("signup.ejs");
 });
 
-app.listen(3000,()=>{
-    console.log("Server running at port 3000 ");
-})
+app.post("/login", (req, res) => {});
+
+app.listen(3000, () => {
+	console.log("Server running at port 3000 ");
+});
