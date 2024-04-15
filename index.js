1c90dfaf2c25c1ab5543a9aa13f80c1c35053a73
sudheervobili <sudheervobillisetty2003@gmail.com>
2024-04-15 15:19:13 +0530

    change

--- index.js
+++ index.js
@@ -28,23 +28,31 @@ mongoose
 	});
 
 app.get("/", (req, res) => {
-	res.render("home", { loginstatus: req.session.status , adminstatus : false});
+	res.render("home", { loginstatus: req.session.status, adminstatus: false });
 });
 
 app.get("/home", (req, res) => {
-	res.render("home", { loginstatus: req.session.status ,adminstatus : false });
+	res.render("home", { loginstatus: req.session.status, adminstatus: false });
 });
 
 app.get("/signup", (req, res) => {
-	res.render("signup", { emailexists: false, mobileexists: false , adminstatus : false});
+	res.render("signup", {
+		emailexists: false,
+		mobileexists: false,
+		adminstatus: false,
+	});
 });
 
 app.get("/login", (req, res) => {
-	res.render("login", { loginerror: false , adminstatus : false});
+	res.render("login", {
+		loginerror: false,
+		adminstatus: false,
+		signupstatus: false,
+	});
 });
 
 app.get("/adminlogin", (req, res) => {
-	res.render("adminlogin", { loginerror: false ,adminstatus : false});
+	res.render("adminlogin", { loginerror: false, adminstatus: false });
 });
 
 app.post("/signup", async (req, res) => {
@@ -53,11 +61,19 @@ app.post("/signup", async (req, res) => {
 	const checkmobile = await userdatamodel.findOne({ mobile: mobile });
 
 	if (checkemail) {
-		return res.render("signup", { emailexists: true, mobileexists: false , adminstatus : false});
+		return res.render("signup", {
+			emailexists: true,
+			mobileexists: false,
+			adminstatus: false,
+		});
 	}
 
 	if (checkmobile) {
-		return res.render("signup", { emailexists: false, mobileexists: true ,adminstatus : false });
+		return res.render("signup", {
+			emailexists: false,
+			mobileexists: true,
+			adminstatus: false,
+		});
 	}
 
 	const hashedpwd = await bcryptjs.hash(password, 12);
@@ -69,7 +85,11 @@ app.post("/signup", async (req, res) => {
 		password: hashedpwd,
 	});
 	await newuser.save();
-	res.render("login", { signupstatus: true, loginerror: false ,adminstatus : false});
+	res.render("login", {
+		signupstatus: true,
+		loginerror: false,
+		adminstatus: false,
+	});
 });
 
 app.post("/login", async (req, res) => {
@@ -77,12 +97,20 @@ app.post("/login", async (req, res) => {
 	const user = await userdatamodel.findOne({ email: email });
 
 	if (!user) {
-		return res.render("login", { signupstatus: false, loginerror: true , adminstatus : false});
+		return res.render("login", {
+			signupstatus: false,
+			loginerror: true,
+			adminstatus: false,
+		});
 	}
 
 	const isPasswordValid = await bcryptjs.compare(password, user.password);
 	if (!isPasswordValid) {
-		return res.render("login", { signupstatus: false, loginerror: true ,adminstatus : false});
+		return res.render("login", {
+			signupstatus: false,
+			loginerror: true,
+			adminstatus: false,
+		});
 	}
 	req.session.email = email;
 	req.session.status = true;
@@ -91,38 +119,53 @@ app.post("/login", async (req, res) => {
 
 app.post("/adminlogin", async (req, res) => {
 	const { email, password } = req.body;
-	if(email === 'admin@gmail.com' && password === '123'){
+	if (email === "admin@gmail.com" && password === "123") {
 		req.session.email = email;
 		req.session.status = true;
-		res.render('adminpage',{adminstatus : true,loginstatus : false,adminstatus : true})
-	}else{
-		res.render('adminlogin',{loginerror : true , adminstatus : false})
+		res.render("adminpage", {
+			adminstatus: true,
+			loginstatus: false,
+			adminstatus: true,
+		});
+	} else {
+		res.render("adminlogin", { loginerror: true, adminstatus: false });
 	}
 });
 
-app.get('/editprofile',async(req,res)=>{
+app.get("/editprofile", async (req, res) => {
 	let email = req.session.email;
-	const userdetails = await userdatamodel.findOne({email});
-	res.render('editprofile',{loginstatus : req.session.status,adminstatus : false,userdetails : userdetails,emailexist : false,mobileexist : false});
+	const userdetails = await userdatamodel.findOne({ email });
+	res.render("editprofile", {
+		loginstatus: req.session.status,
+		adminstatus: false,
+		userdetails: userdetails,
+		emailexist: false,
+		mobileexist: false,
+	});
 });
 
-app.post('/updateprofile', async (req, res) => {
-    const { newname,newmobile, newaddress } = req.body;
+app.post("/updateprofile", async (req, res) => {
+	const { newname, newmobile, newaddress } = req.body;
 	let email = req.session.email;
-    let existingUser = await userdatamodel.findOne({ email });
-	let checkingmobile = await userdatamodel.findOne({mobile : newmobile});
+	let existingUser = await userdatamodel.findOne({ email });
+	let checkingmobile = await userdatamodel.findOne({ mobile: newmobile });
 
-	if(existingUser){
+	if (existingUser) {
 		existingUser.name = newname;
 		existingUser.mobile = newmobile;
 		existingUser.address = newaddress;
 		await existingUser.save();
 	}
-    const userdetails = await userdatamodel.findOne({ email });
+	const userdetails = await userdatamodel.findOne({ email });
 
-    res.render('editprofile', {loginstatus: req.session.status,adminstatus : false,userdetails: userdetails,mobileexist: false,updatedstatus: true});
+	res.render("editprofile", {
+		loginstatus: req.session.status,
+		adminstatus: false,
+		userdetails: userdetails,
+		mobileexist: false,
+		updatedstatus: true,
+	});
 });
-
 
 app.get("/logout", (req, res) => {
 	req.session.destroy((err) => {
